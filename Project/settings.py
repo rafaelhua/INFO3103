d6e1fc60121661f8c4aca4996ecc80d6c3e923bc
RafaelHua <huarafael@gmail.com>
2020-03-09 21:32:30 -0300

    add settings.py

---
+++ Project/settings.py
@@ -0,0 +1,13 @@
+#/usr/bin/env python
+APP_HOST = 'info3103.cs.unb.ca'
+APP_PORT =  51328	
+APP_DEBUG = True
+
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'jhua'
+MYSQL_PASSWD = 'AD09nZCG'
+MYSQL_DB = 'jhua'
+
+SECRET_KEY = 'd41d8cd98f00b204e9800998ecf8427f'
+
+LDAP_HOST =  'ldap-student.cs.unb.ca'
